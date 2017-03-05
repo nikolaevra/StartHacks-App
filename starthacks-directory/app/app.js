670e72f06eccd9f73bfe108f0a9d6c3a964741fc
webstersteele <webster.steele@rogers.com>
2017-03-04 22:42:12 -0500

    yup

--- starthacks-directory/app/app.js
+++ starthacks-directory/app/app.js
@@ -24,6 +24,13 @@
                 controller: 'Account.IndexController',
                 controllerAs: 'vm',
                 data: { activeTab: 'account' }
+            })
+            .state('profile', {
+                url: '/profile',
+                templateUrl: 'profile/index.html',
+                controller: 'profile.IndexController',
+                controllerAs: 'vm',
+                data: { activeTab: 'profile' }
             });
     }
 
